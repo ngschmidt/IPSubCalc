7670476113f43424abc280232448561a3c09b1cb
G89390 <G89390@lak1pce00240793.northgrum.com>
2013-06-05 13:55:27 -0800

    added all basic functions for netmask conversion

--- src/ipsubcalc/IPSubCalc.java
+++ src/ipsubcalc/IPSubCalc.java
@@ -1,19 +1,82 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ipsubcalc;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *test
+ * IP Subnet Calculating Algorithms
  * @author G89390
  */
 public class IPSubCalc {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static byte[] bitwiseAnd(byte[] one, byte[] two) {
+        if(one.length == two.length) {
+            for(int i=0; i<one.length; i++) {
+                one[i] &= two[i];
+            }
+        }
+        return one;
+    }
+    public static byte writeByByte(int a) {
+        switch(a){
+            case 1:
+                return (byte) 128;
+            case 2:
+                return (byte) 192;
+            case 3:
+                return (byte) 224;
+            case 4:
+                return (byte) 240;
+            case 5:
+                return (byte) 248;
+            case 6:
+                return (byte) 252;
+            case 7:
+                return (byte) 254;
+            case 8:
+                return (byte) 255;
+            default: 
+                return (byte) 0;
+        }
+    }
+    public static byte[] writeByNum(int a) {
+        int retLen = (int) Math.ceil((double)a/(double)8);
+        System.out.println(retLen);
+        byte[] ret = new byte[retLen+1];
+        for(int i = 0; i <= retLen; i++){
+            if(a >= 8) {
+                ret[i] = writeByByte(8);
+                a -=8;
+            } else ret[i] = writeByByte(a%8);
+        }
+        return ret;
+    }
+    public static void printBytes(byte[] a) {
+        for(int i = 0; i < a.length; i++)
+            System.out.println(a[i]&0xFF);
+    }
+    public static InetAddress getNetworkAddressByteWise(byte[] addr, byte[] netmask) {
+        try {
+            printBytes(bitwiseAnd(addr, netmask));
+            return InetAddress.getByAddress(bitwiseAnd(addr, netmask));
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(IPSubCalc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return InetAddress.getLoopbackAddress();
+    }
+    public static InetAddress getNetworkAddress(InetAddress addr, byte[] netmask) {
+        return getNetworkAddressByteWise(addr.getAddress(), netmask);        
+    }
+    public static InetAddress getNetworkAddress(InetAddress addr, int netmask) {        
+        return getNetworkAddressByteWise(addr.getAddress(), writeByNum(netmask));        
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            System.out.println(getNetworkAddress(InetAddress.getByName("192.168.33.1"), 22));
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(IPSubCalc.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
