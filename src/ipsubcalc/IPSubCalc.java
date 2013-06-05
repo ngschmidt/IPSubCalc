f6067dcd39aed164addc66eebb9a51737f4ef022
G89390 <G89390@lak1pce00240793.northgrum.com>
2013-06-05 11:45:04 -0800

    initial commit

---
+++ src/ipsubcalc/IPSubCalc.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipsubcalc;
+
+/**
+ *
+ * @author G89390
+ */
+public class IPSubCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
